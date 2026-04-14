00885e388e540a583dbf5155baf3cea1c604bca9
Phillipa2 <phillipaatieno7@gmail.com>
2026-04-14 13:01:44 -0700

    installed react-loader-spinner so as to load before showing the output, and axios was also integrated to  show the weather output of a city

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello from React</h1>
         <Weather city="Nairobi"/>
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
+import {Grid} from "react-loader-spinner";
 
 export default function Weather(props) {
+   const [loading, setLoading] = useState(true);
    
     function handleResponse(response) {
       alert(
-        `The weather in ${response.data.name} is ${response.data.main.temp}℃`
+        `The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}℃`
 );
+     setLoading(false);
 }
-let apiKey = "25fb3dce5bdd0825104d2cf9360c6708";
+if (loading) {
+  let apiKey = "25fb3dce5bdd0825104d2cf9360c6708";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(handleResponse);
 return (
-<div>
-    <h1>Hello World</h1>
-</div>
-)
+  <Grid
+visible={true}
+height="80"
+width="80"
+color="#4fa94d"
+ariaLabel="grid-loading"
+radius="12.5"
+wrapperStyle={{}}
+wrapperClass="grid-wrapper"
+/>
+);
+}
+
 }
